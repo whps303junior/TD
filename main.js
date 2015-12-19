105abdb8b49e237a1a1bd21f42b3cda20ca7040b
whps303junior <whps303junior@gmail.com>
2015-12-19 12:17:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
+var cursor={x:0,y:0};
+
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var heroImg = document.createElement("img");
@@ -10,7 +12,12 @@ var hero = {
   x: 0,
   y: 0
 };
-
+$("game-canvas").mousemove(function(event){
+  cursor.x=event.pageX;
+  cursor.y=event.pageY;
+}
+);
+}
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(heroImg, hero.x, hero.y);
